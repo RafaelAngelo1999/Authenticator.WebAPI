ef6f6e758faa9cb3a9022c4f64faf81820b0bfcd
Rafael <rafaelangelowow@gmail.com>
2022-02-16 21:11:14 -0300

    env

--- Authenticator.WebAPI/Program.cs
+++ Authenticator.WebAPI/Program.cs
@@ -20,17 +20,17 @@ IConfiguration configuration = builder.Configuration;
 IWebHostEnvironment environment = builder.Environment;
 // Add services to the container.
 
-builder.Services.AddControllers(options => options.Filters.Add(typeof (HttpGlobalExceptionFilter)));
+builder.Services.AddControllers(options => options.Filters.Add(typeof(HttpGlobalExceptionFilter)));
 builder.Services.AddEndpointsApiExplorer();
 
 var key = Encoding.ASCII.GetBytes("d41d8cd98f00b204e9800998ecf8427e");
 builder.Services.AddAuthentication(x => { x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme; x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme; }).AddJwtBearer(x => { x.RequireHttpsMetadata = false; x.SaveToken = true; x.TokenValidationParameters = new TokenValidationParameters { ValidateIssuerSigningKey = true, IssuerSigningKey = new SymmetricSecurityKey(key), ValidateIssuer = false, ValidateAudience = false }; });
 
 var serverVersion = new MySqlServerVersion(new Version(10, 4, 22));
-var host = Environment.GetEnvironmentVariable("DBHOST") ?? configuration.GetConnectionString("DBHOST");
-var password = Environment.GetEnvironmentVariable("MYSQL_PASSWORD") ?? configuration.GetConnectionString("MYSQL_PASSWORD");
-var userid = Environment.GetEnvironmentVariable("MYSQL_USER") ?? configuration.GetConnectionString("MYSQL_USER");
-var usersDataBase = Environment.GetEnvironmentVariable("MYSQL_DATABASE") ?? configuration.GetConnectionString("MYSQL_DATABASE");
+var host = configuration.GetConnectionString("DBHOST");
+var password = configuration.GetConnectionString("MYSQL_PASSWORD");
+var userid = configuration.GetConnectionString("MYSQL_USER");
+var usersDataBase = configuration.GetConnectionString("MYSQL_DATABASE");
 
 var connString = $"Server={host};DataBase={usersDataBase};Uid={userid};Pwd={password}";
 builder.Services.AddDbContext<AppDbContext>(options => options.UseMySql(connString, serverVersion));
@@ -40,16 +40,16 @@ builder.Services.AddSwaggerGen(c =>
 
       c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
       {
-        Description = @"JWT Authorization header using the Bearer scheme. 
+          Description = @"JWT Authorization header using the Bearer scheme. 
                       Enter 'Bearer' [space] and then your token in the text input below.
                       Example: 'Bearer 12345abcdef'",
-         Name = "Authorization",
-         In = ParameterLocation.Header,
-         Type = SecuritySchemeType.ApiKey,
-         Scheme = "Bearer"
-       });
+          Name = "Authorization",
+          In = ParameterLocation.Header,
+          Type = SecuritySchemeType.ApiKey,
+          Scheme = "Bearer"
+      });
 
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+      c.AddSecurityRequirement(new OpenApiSecurityRequirement()
       {
         {
           new OpenApiSecurityScheme
@@ -67,7 +67,7 @@ builder.Services.AddSwaggerGen(c =>
             new List<string>()
           }
         });
-});
+  });
 
 #region Injeção
 
